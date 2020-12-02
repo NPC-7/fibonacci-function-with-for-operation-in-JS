7300c6234657515a8e72562f31cfaaf3e752d34c
NPC-7 <75063258+NPC-7@users.noreply.github.com>
2020-12-02 09:05:37 +0700

    adding index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    
+    //Write your code here:
+    
+    var output=[];
+    
+    for(var i=0;i<n;i++){
+        if(i>1){
+            output.push(output[i-1]+output[i-2]);
+        }else if(i===0 || i===1){
+            output.push(i);
+        }
+    }
+    
+    return output;
+    
+    
+    
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
+}
+
